096ca7abaf9b22703862d47fa736bca4a21150c5
Surya Nimmagadda <nscsekhar@juniper.net>
2018-11-11 21:31:37 -0800

    Add multiclass module and unit test

--- code/lstm_multiclass.py
+++ code/lstm_multiclass.py
@@ -9,19 +9,20 @@ from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM
 from keras.preprocessing import sequence
 from keras.preprocessing import text
+from keras.utils import to_categorical
 
 import tensorflow as tf
 from tensorflow.python.client import device_lib
 
 import pickle
 
-class LSTMBinary:
+class LSTMMulti:
 
     def __init__(self):
         
         self.max_features = 1000                                # length of vocabulary
         self.batch_size = 128                                   # input batch size
-        self.num_epochs = 5                                     # epochs to train
+        self.num_epochs = 2                                     # epochs to train
         self.encoder = text.Tokenizer(num_words=500, char_level=True)
 
         self.model = Sequential()
@@ -31,22 +32,24 @@ class LSTMBinary:
         self.encoder.fit_on_texts(X_train)                                    # build character indices
         X_train_tz = self.encoder.texts_to_sequences(X_train)
 
+        self.categories = list(Y_train.unique())
+
         # Model definition - this is the core model from Endgame
         self.model.add(Embedding(self.max_features, 128, input_length=75))
         self.model.add(LSTM(128))
         self.model.add(Dropout(0.5))
-        self.model.add(Dense(1))
-        self.model.add(Activation('sigmoid'))
-        self.model.compile(loss='binary_crossentropy', optimizer='rmsprop')
+        self.model.add(Dense(len(self.categories)))
+        self.model.add(Activation('softmax'))
+        self.model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
         # Pad sequence where sequences are case insensitive characters encoded to
         # integers from 0 to number of valid characters
         X_train_pad=sequence.pad_sequences(X_train_tz, maxlen=75)
-        '''
-        # Train where Y_train is 0-1
-        self.model.fit(X_train_pad, Y_train, batch_size=self.batch_size, epochs=self.num_epochs)
-        '''
-    def save(self, tokenizer_file, model_json_file, model_h5_file):
+        Y_train_binarized = to_categorical(Y_train, num_classes=len(self.categories))
+
+        self.model.fit(X_train_pad, Y_train_binarized, batch_size=self.batch_size, epochs=self.num_epochs)
+
+    def save(self, tokenizer_file, categories_file, model_json_file, model_h5_file):
         #
         # Save the tokenizer
         #
@@ -56,7 +59,6 @@ class LSTMBinary:
         #
         # Save the model and the weights
         #
-        '''
         model_save = self.model.to_json()
         with open(model_json_file, 'w') as file:
             file.write(model_save)
@@ -64,16 +66,21 @@ class LSTMBinary:
         self.model.save_weights(model_h5_file)
 
         print('MODEL SAVED TO DISK!')
-        '''
         pass
 
-    def load(self, tokenizer_file, model_json, model_h5):
+    def load(self, tokenizer_file, categories_file, model_json, model_h5):
         #
         # Load the tokenizer
         #
         with open(tokenizer_file, 'rb') as handle:
             self.encoder = pickle.load(handle)
         
+        #
+        # Load the category list
+        #
+        with open(categories_file, 'rb') as handle:
+            self.categories = pickle.load(handle)
+
         #
         # Load the model and its weights
         #
@@ -82,15 +89,16 @@ class LSTMBinary:
         file.close()
         self.model = model_from_json(model_load)
         self.model.load_weights(model_h5)
-        global graph
-        graph = tf.get_default_graph()
+        global lstm_multi_graph
+        lstm_multi_graph = tf.get_default_graph()
 
-        print('SAVED BINARY MODEL IS NOW LOADED!')
+        print('SAVED MULTICLASS MODEL IS NOW LOADED!')
 
 
     def predict(self, input):
         print(input)
         inputSeq = sequence.pad_sequences(self.encoder.texts_to_sequences(input), maxlen=75)
-        with graph.as_default():
+        with lstm_multi_graph.as_default():
             output = self.model.predict_classes(inputSeq)
-        return output
+        print(output)
+        return self.categories[output]
---
+++ code/test/lstm_multi_test.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+
+REFPATH = "./"
+PROJECT_ROOT = "/Users/nscsekhar/Desktop/nscsekhar/Desktop/Surya/Personal/MIDS/W210/Project/team_cyber/"
+TOKENIZER_FILE = PROJECT_ROOT + "code/saved_models/tokenizer"
+CATEGORIES_FILE = PROJECT_ROOT + "code/saved_models/multiclass_categories"
+MODEL_JSON = PROJECT_ROOT + "code/saved_models/multiclass_LSTM.json"
+MODEL_H5 = PROJECT_ROOT + "code/saved_models/multiclass_LSTM.h5"
+dga_csv = PROJECT_ROOT + "data/2018_0923/dga-feed-high.csv"
+cisco_csv = PROJECT_ROOT + "data/2018_0923/top-1m.csv"
+SUFFIXES = [' DGA', ' (', ' -']
+
+import sys
+sys.path.append(REFPATH)
+import lstm_multiclass 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import pickle
+
+#
+# Prepare dataset (move to a different source)
+#
+""" Extract the DGA categories from the description string """
+def strip_cat(input_str_row, lstrip_str="Domain used by ", rtrunc_str=SUFFIXES, verbose=False):
+    if verbose:
+        print('-'*50, '\nInput:    ', input_str_row['dga'])
+    str1 = input_str_row['dga'].replace(lstrip_str, '')
+    if verbose:
+        print('Lstrip:   ', str1)
+    str2 = str1
+    for i in rtrunc_str:
+        idx = str2.find(i)
+        if idx != -1:
+            str2 = str2[0:idx]
+            if verbose:
+                print('Trimmed:  ', str2)
+            break
+    return str2
+
+def update_categories(input_row):
+    if input_row['dga'] in MERGED_CAT_LIST:
+        return MERGED_CAT_STR
+    else:
+        return input_row['dga']
+
+
+def prepDataset(dga_file, cisco_file):
+    dga_df = pd.read_csv(dga_file, header=None, skiprows=15)
+    cisco_df = pd.read_csv(cisco_file, header=None)
+
+    dga_df_slim =   dga_df.drop(columns=range(2,dga_df.shape[1]), inplace=False)
+    dga_df_slim.columns = ['domain', 'dga']
+    dga_df_slim['dga'] = dga_df_slim.apply(lambda row: strip_cat(row, verbose=False), axis=1)
+    # dga_df_slim['dga'] = dga_df_slim.apply(update_categories, axis=1)
+
+    cisco_df_slim = cisco_df.drop(columns=[0], inplace=False)
+    cisco_df_slim.columns = ['domain']
+    cisco_df_slim['dga'] = 'nonDGA'
+
+    unified_df = pd.concat([cisco_df_slim, dga_df_slim], ignore_index=True)
+    unified_df['catIndex'], labels = pd.factorize(unified_df['dga'], sort=True)
+
+    print(labels)
+
+    with open(CATEGORIES_FILE, 'wb') as handle:
+        pickle.dump(labels, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    X = unified_df['domain']
+    Y = unified_df['catIndex']
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2,random_state=23)
+
+    return X_train, X_test, Y_train, Y_test
+
+#
+# Get the train and test data
+#
+X_train, X_test, Y_train, Y_test = prepDataset(dga_csv, cisco_csv)
+
+#
+# Train the model
+#
+train_model = lstm_multiclass.LSTMMulti()
+train_model.train(X_train, Y_train)
+train_model.save(TOKENIZER_FILE, CATEGORIES_FILE, MODEL_JSON, MODEL_H5)
+
+#
+# Test
+#
+testmodel = lstm_multiclass.LSTMMulti()
+testmodel.load(TOKENIZER_FILE, CATEGORIES_FILE, MODEL_JSON, MODEL_H5)
+
+urllist = ["www.google.com", "www.netflix.com", "plvklpgwivery.com"]
+urltypes = testmodel.predict(urllist)
+print("URL type:", urltypes)
